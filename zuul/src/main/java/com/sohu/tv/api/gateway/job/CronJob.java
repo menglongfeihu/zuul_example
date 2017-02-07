54ade499fa025bef88a3614c8b1ad1687b4f8244
李安增(视频) <anzengli@sohu-inc.com>
2017-02-07 11:51:17 +0800

    新增 监控mysql zipkin跟踪

--- zuul/src/main/java/com/sohu/tv/api/gateway/job/CronJob.java
+++ zuul/src/main/java/com/sohu/tv/api/gateway/job/CronJob.java
@@ -7,17 +7,20 @@ import javax.annotation.Resource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.sohu.tv.api.gateway.beans.MysqlBean;
 
-//@Configuration
-//@EnableScheduling // 启用定时任务
+@Configuration
+@EnableScheduling // 启用定时任务
 public class CronJob {
     protected final Log logger = LogFactory.getLog(getClass());
     @Resource
     MysqlBean mysqlBean;
 
-    //@Scheduled(cron = "0/20 * * * * ?") // 每20秒执行一次
+    @Scheduled(cron = "0/5 * * * * ?") // 每20秒执行一次
     public void scheduler() {
         logger.info(">>>>>>>>>>>>> scheduled ... ");
         mysqlBean.test();
